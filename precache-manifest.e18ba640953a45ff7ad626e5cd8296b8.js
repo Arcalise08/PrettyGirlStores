65586e31102f204c446ee077f2a2d0eab50a300f
Arcalise08 <demonata08@gmail.com>
2020-11-30 17:22:43 -0600

    Updates

---
+++ precache-manifest.e18ba640953a45ff7ad626e5cd8296b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e57ae9e8b71fe040f9ea",
+    "url": "/PrettyGirlStores/static/css/main.b9bbc612.chunk.css"
+  },
+  {
+    "revision": "e57ae9e8b71fe040f9ea",
+    "url": "/PrettyGirlStores/static/js/main.e57ae9e8.chunk.js"
+  },
+  {
+    "revision": "36631b12b9988dcf3fe7",
+    "url": "/PrettyGirlStores/static/js/runtime~main.36631b12.js"
+  },
+  {
+    "revision": "d96d9cec1e3007a2bcde",
+    "url": "/PrettyGirlStores/static/css/2.07c6e62a.chunk.css"
+  },
+  {
+    "revision": "d96d9cec1e3007a2bcde",
+    "url": "/PrettyGirlStores/static/js/2.d96d9cec.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/PrettyGirlStores/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "5febb297ebe8dce10ec882ff3efb8536",
+    "url": "/PrettyGirlStores/index.html"
+  }
+];
